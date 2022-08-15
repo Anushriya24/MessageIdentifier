da4c5a9f4b255923669ac59281689e3243263af5
Anushriya24 <anushriyaroy24@gmail.com>
2022-08-15 17:34:19 +0530

    Minor Improvements

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-# before NORMALIZATION/SCALING
-
+# Before NORMALIZATION/SCALING
 import numpy as np
 import matplotlib.pyplot as plt
 np.random.seed(0)
@@ -7,6 +6,7 @@ x1 = np.random.randint(1,50,30)
 x1 = np.sort(x1)
 x2 = np.random.randint(10000,70000,30)
 plt.plot(x1,x2)
+
 # after NORMALIZATION/SCALING
 x1min = min(x1)
 x1max = max(x1)
@@ -15,11 +15,13 @@ x2max = max(x2)
 x1norm = (x1-x1min)/(x1max-x1min)
 x2norm = (x2-x2min)/(x2max-x2min)
 plt.plot(x1norm,x2norm)
+
 #LOGISTIC REGRESSION - IT IS A CLASSIFICATION TECHNIQUE
 import pandas as pd
 df = pd.read_csv('https://raw.githubusercontent.com/ameenmanna8824/DATASETS/main/Social_Network_Ads.csv')
 df
 df.info()
+
 #divide into i/p and o/p
 x = df.iloc[:,2:4].values #2 dimensional
 y = df.iloc[:,4].values #1 dimensional
@@ -32,11 +34,13 @@ x_train,x_test,y_train,y_test = train_test_split(x,y,random_state = 0)
 x.shape
 x_train.shape
 x_test.shape
+
 #scaling or normalization(ONLY FOR INPUTS)
 from sklearn.preprocessing import MinMaxScaler
 scaler = MinMaxScaler()
 x_train = scaler.fit_transform(x_train)
 x_test = scaler.fit_transform(x_test)
+
 #apply REGRESSOR/CLASSIFIER
 from sklearn.linear_model import LogisticRegression
 model = LogisticRegression()
@@ -45,9 +49,11 @@ model.fit(x_train,y_train)
 y_pred = model.predict(x_test)
 y_pred
 y_test
+
 #to check the accuracy
 from sklearn.metrics import accuracy_score
 accuracy_score(y_pred,y_test)*100
+
 #SUPPORT VECTOR MACHINE
 #https://github.com/diazoniclabs - for dataset
 import pandas as pd
@@ -56,6 +62,7 @@ df
 df.info()
 #the count of spam and ham messages
 df['label'].value_counts()
+
 #VISUALIZATION
 df['label'].value_counts().plot(kind = 'bar')
 #divide into i/p and o/p
@@ -64,6 +71,7 @@ y = df.iloc[:,0].values
 #train_test_split
 from sklearn.model_selection import train_test_split
 x_train,x_test,y_train,y_test = train_test_split(x,y,random_state = 0)
+
 #Apply TFIDF Vectorizer - Very imp for text based dataset
 from sklearn.feature_extraction.text import TfidfVectorizer
 vect = TfidfVectorizer()
